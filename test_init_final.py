c245cd9bfb75fe2105ba1f3c21ace65ed41ecd2e
minedkboss <112678806+minedkboss@users.noreply.github.com>
2022-09-03 10:25:07 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -3882,7 +3882,7 @@ class mainCog(commands.Cog):
 
 class IlsangDistributionBot(commands.AutoShardedBot):
 	def __init__(self):
-		super().__init__(command_prefix=[""], intents=intents)
+		super().__init__(command_prefix=[""], intents=discord.Intents.default())
 
 	def run(self):
 		super().run(access_token, reconnect=True)
